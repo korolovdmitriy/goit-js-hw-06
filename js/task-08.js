32d683ae76ff855770bb7019acb05b7addcba9ad
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-12 12:40:41 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onLoginFormSubmit);
+
+function onLoginFormSubmit(event) {
+    event.preventDefault();
+    const dataFormResult = {};
+    
+    if (event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === '') return alert('все поля должны быть заполнены.');
+ 
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((value, name) => {
+        dataFormResult[name] = value;
+        
+    });
+    
+    console.log(dataFormResult);
+    loginForm.reset();
+}
