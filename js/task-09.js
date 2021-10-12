e9c6c5dcfd4d102770477b03eec4e99a6305e634
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-12 14:03:09 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('button.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('span.color')
+
+buttonEl.addEventListener('click', onButtonElClick);
+
+function onButtonElClick() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
